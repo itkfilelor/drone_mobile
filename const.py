033d8c7100884872a5e1d39d5a65dc685eb1b896
itkfilelor <wowbutters@gmail.com>
2021-01-25 10:10:15 -0500

    Create const.py

---
+++ const.py
@@ -0,0 +1,29 @@
+AWSCLIENTID = "3l3gtebtua7qft45b4splbeuiu"
+
+URLS = {
+    "auth":"https://cognito-idp.us-east-1.amazonaws.com/",
+    "user_info":"https://api.dronemobile.com/api/v1/user",
+    "vehicle_info":"https://api.dronemobile.com/api/v1/vehicle?limit=",
+    "command":"https://accounts.dronemobile.com/api/iot/send-command"}
+AVAILABLE_COMMANDS = [
+    'trunk',
+    'remote_start',
+    'remote_stop',
+    'arm',
+    'disarm',
+    'panic',
+    'remote_aux1',
+    'remote_aux2',
+    'location'
+    ]
+COMMAND_HEADERS = {
+                    'x-drone-api': None,
+                    'Content-Type': 'application/json;charset=utf-8'
+                }
+
+AUTH_HEADERS = {
+                  'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
+                  'Content-Type': 'application/x-amz-json-1.1'
+               }
+
+resourceDir = 'resources'
